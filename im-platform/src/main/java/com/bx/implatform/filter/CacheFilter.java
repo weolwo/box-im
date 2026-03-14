b3535a1f047e844fd654c7c22bbdc85e96fff5f1
admin <admin@qq.com>
2026-03-14 14:13:11 +0800

    1.优化CacheHttpServletRequestWrapper 中的判断是否读完和可读的数据 2.过滤器中 只对请求类型是APPLICATION_JSON_VALUE 进行拦截，避免如文件上传等OOM

--- im-platform/src/main/java/com/bx/implatform/filter/CacheFilter.java
+++ im-platform/src/main/java/com/bx/implatform/filter/CacheFilter.java
@@ -3,14 +3,16 @@ package com.bx.implatform.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
 @ServletComponentScan
-@WebFilter(urlPatterns = "/*", filterName = "xssFilter")
+@WebFilter(urlPatterns = "/*", filterName = "cacheFilter")
 public class CacheFilter implements Filter {
 
     @Override
@@ -22,8 +24,18 @@ public class CacheFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        chain.doFilter(new CacheHttpServletRequestWrapper((HttpServletRequest) request), response);
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        ServletRequest requestWrapper = null;
+        if (request instanceof HttpServletRequest
+                && StringUtils.startsWithIgnoreCase(request.getContentType(), MediaType.APPLICATION_JSON_VALUE)) {
+            requestWrapper = new CacheHttpServletRequestWrapper((HttpServletRequest) request);
+        }
+        if (null == requestWrapper) {
+            chain.doFilter(request, response);
+        } else {
+            chain.doFilter(requestWrapper, response);
+        }
     }
 
 }
--- im-platform/src/main/java/com/bx/implatform/filter/CacheHttpServletRequestWrapper.java
+++ im-platform/src/main/java/com/bx/implatform/filter/CacheHttpServletRequestWrapper.java
@@ -8,6 +8,11 @@ import org.apache.commons.compress.utils.IOUtils;
 
 import java.io.*;
 
+/**
+ *
+ * 的核心逻辑是：把前端传过来的整个请求体（Body）读取出来，存到一个内存数组 byte[] requestBody 里。它的初衷是为了解决 “流只能读取一次” 的问题
+ * （因为后面的参数防抖切面、日志切面、甚至是 Controller 里的 @RequestBody 都需要反复读这个流）。
+ */
 public class CacheHttpServletRequestWrapper extends HttpServletRequestWrapper {
     private byte[] requestBody;
     private final HttpServletRequest request;
@@ -29,7 +34,13 @@ public class CacheHttpServletRequestWrapper extends HttpServletRequestWrapper {
         return new ServletInputStream() {
             @Override
             public boolean isFinished() {
-                return false;
+                return bais.available() == 0;
+                //return false;  永远不结束
+            }
+
+            @Override
+            public int available() throws IOException {
+                return bais.available();
             }
 
             @Override
